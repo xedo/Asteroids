cdf2ec3162ae74d7e43d4f8697967bbe614aca27
Echedey Lima <echedeylima@gmail.com>
2013-04-08 22:42:14 +0100

    Creado hilo de fisica

--- Asteroids/src/com/echedeylima/asteroids/VistaJuego.java
+++ Asteroids/src/com/echedeylima/asteroids/VistaJuego.java
@@ -21,6 +21,20 @@ public class VistaJuego extends View {
 	private Vector<Grafico> Asteroides; // Vector con los asteroides
 	private int numAsteroides = 5; // Numero inicial de asteroides
 	private int numFragmentos = 3; // Fragmentos en los que se divide
+	
+	// Thread
+	private ThreadJuego thread = new ThreadJuego();
+	private static int PERIODO_PROCESO = 50;
+	private long ultimoProceso = 0;
+	
+	public class ThreadJuego extends Thread {
+		@Override
+		public void run(){
+			while (true){
+				actualizaFisica();
+			}
+		}
+	}
 
 	public VistaJuego(Context context, AttributeSet attrs) {
 		super(context, attrs);
@@ -53,6 +67,30 @@ public class VistaJuego extends View {
 			Asteroides.add(asteroide);
 		}
 	}
+	
+	protected void actualizaFisica() {
+		long ahora = System.currentTimeMillis();
+		//para la ejecucion si aun no se ha pumplido el periodo de proceso
+		if (ultimoProceso + PERIODO_PROCESO > ahora)
+			return;
+		//para una ejecucion en tiempo real calculamos el retador
+		double retardo = (ahora - ultimoProceso) / PERIODO_PROCESO;
+		ultimoProceso = ahora;
+		//actualizamos la velocidad y direcci—n de la nave a partir de grioNave y acelereacionNave (segun la entrada del jugador)
+		nave.setAngulo((int) (nave.getAngulo() + giroNave * retardo));
+		double nIncX = nave.getIncX() + aceleracionNave * Math.cos(Math.toRadians(nave.getAngulo())) * retardo;
+		double nIncY = nave.getIncY() + aceleracionNave * Math.sin(Math.toRadians(nave.getAngulo())) * retardo;
+		//actualizamos si el modulo de la velocidad no excede del maximo
+		if (Math.hypot(nIncX, nIncY) <= Grafico.getMaxVelocidad()){
+			nave.setIncX(nIncX);
+			nave.setIncY(nIncY);
+		}
+		//actualizamos posiciones
+		nave.incrementaPos(retardo);
+		for (Grafico asteroide : Asteroides){
+			asteroide.incrementaPos(retardo);
+		}
+	}
 
 	@Override
 	protected void onSizeChanged(int ancho, int alto, int ancho_anter,
@@ -72,6 +110,9 @@ public class VistaJuego extends View {
 				asteroide.setPosY(Math.random() * (alto - asteroide.getAlto()));
 			} while (asteroide.distancia(nave) < (ancho + alto) / 5);
 		}
+		
+		ultimoProceso = System.currentTimeMillis();
+		thread.start();
 	}
 
 	@Override
