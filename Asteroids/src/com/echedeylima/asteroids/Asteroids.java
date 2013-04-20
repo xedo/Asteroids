cd7cf0bcb931199e6b51f14fb8b148ed5773a473
Echedey Lima <echedeylima@gmail.com>
2013-04-20 21:52:32 +0100

    añadidos toast, para ver los ciclos de vida de las actividades

--- Asteroids/src/com/echedeylima/asteroids/Asteroids.java
+++ Asteroids/src/com/echedeylima/asteroids/Asteroids.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 //import android.view.View.OnClickListener;
 //import android.widget.Button;
 
@@ -21,11 +22,50 @@ public class Asteroids extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
+		Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+
 		// bAcercaDe =(Button) findViewById(R.id.button3);
 		// bAcercaDe.setOnClickListener(new OnClickListener(){
 		/*
 		 * public void onClick(View view){ lanzarAcercaDe(null); } });
-		 */ 
+		 */
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Toast.makeText(this, "onStrat", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+
+	}
+
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+		Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
 	}
 
 	@Override
@@ -38,7 +78,7 @@ public class Asteroids extends Activity {
 	}
 
 	@Override
-	public boolean onOptionsItemSelected (MenuItem item) {
+	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case R.id.acercaDe:
 			lanzarAcercaDe(null);
